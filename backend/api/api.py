c0100b08e686e736ddb83ca04c786476cee18f9e
JuampaBaquero <jpablo-baquero@javeriana.edu.co>
2026-05-17 13:23:55 -0500

    Corrected bug when launching the app.

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,9 +1,8 @@
 #Is the router manager
 from fastapi import APIRouter
-from api.endpoints import circuits, gates, simulations
+from api.endpoints import circuits, simulations
 
 #Here I pick up all of the endpoints defined in the route
 api_router = APIRouter()
 api_router.include_router(circuits.router, prefix = "/circuits", tags = ["circuits"])
-api_router.include_router(gates.router, prefix = "/gates", tags = ["gates"])
 api_router.include_router(simulations.router, prefix = "/simulations", tags = ["simulations"])
